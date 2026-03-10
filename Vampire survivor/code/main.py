b79ed85322bfa3a46f6440b3dd382fa1cf2418fc
Agustín Cámara <cm.agustin27@gmail.com>
2026-03-09 23:49:04 -0300

    Vampire Survivor game upload

--- Vampire survivor/code/main.py
+++ Vampire survivor/code/main.py
@@ -1,1 +1,136 @@
+import pygame.mixer
+from pygame import FULLSCREEN
+
 from settings import *
+from player import Player
+from sprites import *
+from pytmx.util_pygame import load_pygame
+from groups import AllSprites
+
+from random import randint, choice
+
+class Game:
+    def __init__(self):
+        # Setup
+        pygame.init()
+        self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Vampire Survivor')
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+        # Groups
+        self.all_sprites = AllSprites()
+        self.collision_sprites = pygame.sprite.Group()
+        self.bullet_sprites = pygame.sprite.Group()
+        self.enemy_sprites = pygame.sprite.Group()
+
+        # Gun timer
+        self.can_shoot = True
+        self.shoot_time = 0
+        self.gun_cooldown = 100
+
+        # Enemy timer
+        self.enemy_event = pygame.event.custom_type()
+        pygame.time.set_timer(self.enemy_event, 3000)
+        self.spawn_positions = []
+
+        # Setup
+        self.load_images()
+        self.load_sounds()
+        self.setup()
+        self.game_music.play(loops = -1)
+
+    def load_images(self):
+        self.bullet_surf = pygame.image.load(join('..', 'images', 'gun', 'bullet.png')).convert_alpha()
+
+        # Enemies
+        folders = list(walk(join('..', 'images', 'enemies')))[0][1]
+        self.enemy_frames = {}
+        for folder in folders:
+            for folder_path, _, file_names in walk(join('..', 'images', 'enemies', folder)):
+                self.enemy_frames[folder] = []
+                for file_name in sorted(file_names, key = lambda name: int(name.split('.')[0])):
+                    full_path = join(folder_path, file_name)
+                    surf = pygame.image.load(full_path).convert_alpha()
+                    self.enemy_frames[folder].append(surf)
+
+    def load_sounds(self):
+        self.kill_sound = pygame.mixer.Sound(join('..', 'audio', 'impact.ogg'))
+        self.kill_sound.set_volume(0.1)
+        self.shoot_sound = pygame.mixer.Sound(join('..', 'audio', 'shoot.wav'))
+        self.shoot_sound.set_volume(0.1)
+        self.game_music = pygame.mixer.Sound(join('..', 'audio', 'music.wav'))
+        self.game_music.set_volume(0.1)
+
+    def input(self):
+        if pygame.mouse.get_pressed()[0] and self.can_shoot:
+            pos = self.gun.rect.center + self.gun.player_direction * 50
+            Bullet(self.bullet_surf, pos, self.gun.player_direction, (self.all_sprites, self.bullet_sprites))
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+            self.shoot_sound.play()
+
+    def gun_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time -self.shoot_time >= self.gun_cooldown:
+                self.can_shoot = True
+
+    def collision(self):
+        # Bullet collision
+        if self.bullet_sprites:
+            for bullet in self.bullet_sprites:
+                collision_sprites = pygame.sprite.spritecollide(bullet, self.enemy_sprites, False, pygame.sprite.collide_mask)
+                if collision_sprites:
+                    self.kill_sound.play()
+                    for sprite in collision_sprites:
+                        sprite.destroy()
+                    bullet.kill()
+
+        # Player collision
+        if self.enemy_sprites:
+            if pygame.sprite.spritecollide(self.player, self.enemy_sprites, False, pygame.sprite.collide_mask):
+                self.running = False
+
+    def setup(self):
+        map = load_pygame(join('..', 'data', 'maps', 'world.tmx'))
+        for x,y,image in map.get_layer_by_name('Ground').tiles():
+            Sprite((x * TILE_SIZE,y * TILE_SIZE), image, self.all_sprites)
+
+        for obj in map.get_layer_by_name('Objects'):
+            CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
+
+        for obj in map.get_layer_by_name('Collisions'):
+            CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), self.collision_sprites)
+
+        for obj in map.get_layer_by_name('Entities'):
+            if obj.name == 'Player':
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+                self.gun = Gun(self.player, self.all_sprites)
+            else:
+                self.spawn_positions.append((obj.x, obj.y))
+
+    def run(self):
+        while self.running:
+            dt = self.clock.tick() / 1000
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                if event.type == self.enemy_event:
+                    Enemy(choice(self.spawn_positions), choice(list(self.enemy_frames.values())), (self.all_sprites, self.enemy_sprites), self.player, self.collision_sprites)
+
+            # Update
+            self.gun_timer()
+            self.input()
+            self.all_sprites.update(dt)
+            self.collision()
+
+            # Draw
+            self.all_sprites.draw(self.player.rect.center)
+            pygame.display.flip()
+        pygame.quit()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
